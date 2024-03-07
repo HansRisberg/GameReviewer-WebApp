d4372e65d2c04eccb79d0f4fd0a219f9628113a5
HansRisberg <123938027+HansRisberg@users.noreply.github.com>
2024-03-07 12:38:39 +0100

    Fixed and issue where the carousel on homepage didn't render properly during the first itteration of the first image.

--- Frontend/gamereviewerreact/src/Components/GameCarousel.js
+++ Frontend/gamereviewerreact/src/Components/GameCarousel.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 
 const gamesData = [
   { title: 'Baldurs Gate 3', imageUrl: '/Assets/bg3.webp' },
   { title: 'Warhammer40k Darktide', imageUrl: '/Assets/darktide2.webp' },
-  // Add more game data as needed
+  // Add more images
 ];
 
 const GameCarousel = () => {
-    return (
-      <div style={{ marginTop: '-40px' , position: 'relative', zIndex: '-1' }}> {/* Apply negative margin for overlap */}
-        <Carousel cycleNavigation>
-          {gamesData.map((game) => (
-            <Paper key={game.title}>
-              <img src={game.imageUrl} alt={game.title} style={{ width: '100%', height: 'auto' }} />
-            </Paper>
-          ))}
-        </Carousel>
-      </div>
-    );
-  };
-  
-  export default GameCarousel;
+  const [imagesLoaded, setImagesLoaded] = useState(0);
 
-  
+  const handleImageLoad = () => {
+    setImagesLoaded((prev) => prev + 1);
+  };
+
+  return (
+    <div style={{ marginTop: '-40px', position: 'relative', zIndex: '-1' }}>
+      <Carousel cycleNavigation>
+        {gamesData.map((game) => (
+          <Paper key={game.title} style={{ display: imagesLoaded === gamesData.length ? 'block' : 'none' }}>
+            <img
+              src={game.imageUrl}
+              alt={game.title}
+              style={{ width: '100%', height: 'auto' }}
+              onLoad={handleImageLoad}
+            />
+          </Paper>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default GameCarousel;
+
+
