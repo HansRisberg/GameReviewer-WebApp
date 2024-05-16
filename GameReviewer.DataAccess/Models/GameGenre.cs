152acb368d56cd1f01a3b2491348f0a1c889817f
hjris <hj.risberg@gmail.com>
2024-05-16 09:32:16 +0200

    changing base url

--- GameReviewer.DataAccess/Models/GameGenre.cs
+++ GameReviewer.DataAccess/Models/GameGenre.cs
@@ -31,33 +31,4 @@ namespace GameReviewer.DataAccess.Models
         }
     }
 }
-//using Microsoft.EntityFrameworkCore;
 
-//namespace GameReviewer.DataAccess.Models {
-//    public class GameCategory {
-//        public int GameCategoryId { get; set; } // Add a unique identifier
-
-//        public int GameId { get; set; }
-//        public Game? Game { get; set; }
-
-//        public int CategoryId { get; set; }
-//        public Category? Category { get; set; }
-
-//        // Other properties related to the association...
-
-//        protected static void OnModelCreating(ModelBuilder modelBuilder) {
-//            modelBuilder.Entity<GameCategory>()
-//                .HasKey(gc => gc.GameCategoryId); // Set GameCategoryId as the primary key
-
-//            modelBuilder.Entity<GameCategory>()
-//                .HasOne(gc => gc.Game)
-//                .WithMany(g => g.GameCategories)
-//                .HasForeignKey(gc => gc.GameId);
-
-//            modelBuilder.Entity<GameCategory>()
-//                .HasOne(gc => gc.Category)
-//                .WithMany(c => c.GameCategories)
-//                .HasForeignKey(gc => gc.CategoryId);
-//        }
-//    }
-//}
