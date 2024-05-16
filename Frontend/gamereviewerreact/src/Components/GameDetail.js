8f6524c7c749ae92ee99528b945292df3ad37f03
hjris <hj.risberg@gmail.com>
2024-05-16 13:20:44 +0200

    CHanging back to local server to test

--- Frontend/gamereviewerreact/src/Components/GameDetail.js
+++ Frontend/gamereviewerreact/src/Components/GameDetail.js
@@ -14,7 +14,7 @@ const GameDetail = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const gameResponse = await axios.get(`https://gamereviewerbackend.azurewebsites.net/api/Games/${id}`);
+        const gameResponse = await axios.get(`https://localhost:7168/api/Games/${id}`);
         console.log('Game Detail Response:', gameResponse.data);
         setGame(gameResponse.data);
 
