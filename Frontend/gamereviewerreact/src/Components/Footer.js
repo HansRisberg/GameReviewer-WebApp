918246504220caf9ccaa6e14dffef4b64cae9944
hjris <hj.risberg@gmail.com>
2024-05-21 14:40:27 +0200

    fixed footer

--- Frontend/gamereviewerreact/src/Components/Footer.js
+++ Frontend/gamereviewerreact/src/Components/Footer.js
@@ -3,10 +3,12 @@ import '../CSS/Footer.css'; /* CSS for styling the footer */
 
 const Footer = () => {
   return (
+    <div className='footer-container'>
     <footer className="footer"> {/* Apply a class for styling */}
       <p>© Example copyright. No rights reserved.</p> {/* Example copyright */}
       <p>Contact us at Example@SomeEmail.com</p> {/* Example contact information */}
     </footer>
+    </div>
   );
 };
 
