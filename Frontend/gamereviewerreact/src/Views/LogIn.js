b397fede5931551577f6de50cb027e94f0a8b534
hjris <hj.risberg@gmail.com>
2024-05-27 10:31:59 +0200

    Fixed footer hiding content. Added login instructions Added dormancy warning in GameList

--- Frontend/gamereviewerreact/src/Views/LogIn.js
+++ Frontend/gamereviewerreact/src/Views/LogIn.js
@@ -4,7 +4,7 @@ const LoginView = () => {
     return(
         <div>
             <h1>Login View</h1>
-            <p>This is where you can log in</p>
+            <p>This is where you can log in, to create a new user click link to register. Or You can login with : Email: TestUser@gmail.com, Password: Test/9 </p>
             <LoginForm />
         </div>
     );
